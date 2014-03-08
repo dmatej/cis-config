80c67fc79653aed4a2b6b118fe337c5564f7ec0f
augustar <augusta.rudolf@gmail.com>
2014-03-08 20:00:28 +0100

    #5 added UserManager.java for working with user. Updated secret.xhtml where is user printed throw userManager. Updated persistance.xml to not droping table when app is deployed.

---
+++ cis-config-war/src/main/java/cz/i/cis/config/web/UserManager.java
@@ -0,0 +1,23 @@
+package cz.i.cis.config.web;
+
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.faces.bean.RequestScoped;
+import javax.inject.Named;
+
+import cz.i.cis.config.ejb.UserDao;
+import cz.i.cis.config.jpa.usr.en.CisUser;
+
+@Named(value = "userManager")
+@RequestScoped
+public class UserManager {
+
+	@EJB
+	private UserDao userDao;
+
+	public List<CisUser> getAllUsers() {
+		return userDao.listUsers();
+	}
+
+}

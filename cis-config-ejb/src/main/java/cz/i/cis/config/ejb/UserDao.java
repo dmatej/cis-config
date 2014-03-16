38e963ebf061a325adc94dd1651625b75af52fde
Mr.FrAnTA <michal.dekany@seznam.cz>
2014-03-16 20:34:58 +0100

    #3 Improved UserDao.
    Now it's possible to get, add, remove and update cis users.

--- cis-config-ejb/src/main/java/cz/i/cis/config/ejb/UserDao.java
+++ cis-config-ejb/src/main/java/cz/i/cis/config/ejb/UserDao.java
@@ -3,6 +3,7 @@
  */
 package cz.i.cis.config.ejb;
 
+import java.util.Date;
 import java.util.List;
 
 import javax.ejb.Local;
@@ -24,8 +25,42 @@ public class UserDao {
   @PersistenceContext(name="cis-jta")
   private EntityManager em;
 
+  public UserDao() {
+  }
+
   public List<CisUser> listUsers() {
-    final TypedQuery<CisUser> query = em.createQuery("select user from CisUser user", CisUser.class);
+    final TypedQuery<CisUser> query = this.em.createQuery("select user from CisUser user", CisUser.class);
+
     return query.getResultList();
+  }
+
+  public void addUser(CisUser user) {
+    this.em.getTransaction().begin();
+    this.em.persist(user);
+    this.em.getTransaction().commit();
+  }
+
+  public void removeUser(CisUser user) {
+    this.em.getTransaction().begin();
+    this.em.remove(user);
+    this.em.getTransaction().commit();
+  }
+
+  public CisUser updateUser(CisUser user) {
+    this.em.getTransaction().begin();
+    CisUser merged = this.em.merge(user);
+    this.em.getTransaction().commit();
+
+    return merged;
+  }
+
+  public static CisUser createUser(String login, String firstName, String lastName, Date birthDate) {
+    CisUser user = new CisUser();
+    user.setLogin(login);
+    user.setFirstName(firstName);
+    user.setLastName(lastName);
+    user.setBirthDate(birthDate);
+
+    return user;
   }
 }

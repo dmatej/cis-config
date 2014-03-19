acf874565529334539035f0d03f032360a27ea68
David Matejcek <david.matejcek@i.cz>
2014-03-19 17:01:34 +0100

    #7 Impemented equals in CisUser
    - hashCode based only on id and login (should not change)
    - equals - birthDate is equal if it is the same day

--- cis-config-ejb/src/main/java/cz/i/cis/config/jpa/CisUser.java
+++ cis-config-ejb/src/main/java/cz/i/cis/config/jpa/CisUser.java
@@ -5,12 +5,16 @@ import java.util.Date;
 
 import javax.persistence.*;
 
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.time.DateUtils;
+
 /**
  * Entity implementation class for Entity: CisUser
  */
 @Entity
 @Table(name = "cisuser")
 public class CisUser implements Serializable {
+  private static final long serialVersionUID = 1L;
 
   @Id
   @Column(nullable = false, updatable = false)
@@ -30,7 +34,6 @@ public class CisUser implements Serializable {
   @Column(nullable = false)
   private Date birthDate;
 
-  private static final long serialVersionUID = 1L;
 
 
   public CisUser() {
@@ -85,5 +88,73 @@ public class CisUser implements Serializable {
 
   public void setBirthDate(Date birthDate) {
     this.birthDate = birthDate;
+  }
+
+
+
+  @Override
+  public String toString() {
+    return ToStringBuilder.reflectionToString(this);
+  }
+
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + (id == null ? 0 : id.hashCode());
+    result = prime * result + (login == null ? 0 : login.hashCode());
+    return result;
+  }
+
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (!(obj instanceof CisUser)) {
+      return false;
+    }
+    CisUser other = (CisUser) obj;
+    if (birthDate == null) {
+      if (other.birthDate != null) {
+        return false;
+      }
+    } else if (!DateUtils.isSameDay(birthDate, other.birthDate)) {
+      return false;
+    }
+    if (firstName == null) {
+      if (other.firstName != null) {
+        return false;
+      }
+    } else if (!firstName.equals(other.firstName)) {
+      return false;
+    }
+    if (id == null) {
+      if (other.id != null) {
+        return false;
+      }
+    } else if (!id.equals(other.id)) {
+      return false;
+    }
+    if (lastName == null) {
+      if (other.lastName != null) {
+        return false;
+      }
+    } else if (!lastName.equals(other.lastName)) {
+      return false;
+    }
+    if (login == null) {
+      if (other.login != null) {
+        return false;
+      }
+    } else if (!login.equals(other.login)) {
+      return false;
+    }
+    return true;
   }
 }

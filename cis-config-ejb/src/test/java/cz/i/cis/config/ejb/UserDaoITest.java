cd534c40533024312326fb9cdf750eb21c9a202e
andrej <pirat.yohoho@gmail.com>
2014-04-08 06:29:12 +0200

    #20 added test for remove user

--- cis-config-ejb/src/test/java/cz/i/cis/config/ejb/UserDaoITest.java
+++ cis-config-ejb/src/test/java/cz/i/cis/config/ejb/UserDaoITest.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 import javax.ejb.EJB;
 
-import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.junit.After;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,26 +20,36 @@ import cz.i.cis.config.ejb.dao.UserAlreadyExistsException;
 import cz.i.cis.config.jpa.CisUser;
 import cz.i.cis.config.test.ArquillianITest;
 
-
 /**
  * @author David Matějček
- *
  */
 public class UserDaoITest extends ArquillianITest {
-  private static final Logger LOG = LoggerFactory.getLogger(UserDaoITest.class);
 
+  private static final Logger LOG = LoggerFactory.getLogger(UserDaoITest.class);
 
   @EJB(mappedName = "java:global/cis-config-test/cis-config-test-ejb/CisUserDao")
   private CisUserDao dao;
 
 
-  @Test(expected=UserAlreadyExistsException.class)
+  @After
+  public void removeEnvirenments() {
+    List<CisUser> users = dao.listUsers();
+    dao.removeUser(users.get(0));
+    List<CisUser> list_users = dao.listUsers();
+    assertTrue("list_users.isEmpty", list_users.isEmpty());
+    LOG.debug("list_users: {}", list_users);
+  }
+
+
+  @Test(expected = UserAlreadyExistsException.class)
   public void createUser() throws UserAlreadyExistsException {
+    LOG.debug("DEBUG CisUserDao");
     CisUser user0 = new CisUser();
     user0.setLastName("Matějka");
     user0.setLogin("xt001");
     user0.setFirstName("Afanasi");
     user0.setBirthDate(new Date());
+    dao.addUser(user0);
     dao.addUser(user0);
     LOG.debug("user0: {}", user0);
     assertNotNull("user0.id", user0.getId());
@@ -52,7 +62,6 @@ public class UserDaoITest extends ArquillianITest {
     assertNotNull("users[0]", user);
     assertEquals("user.hashCode", user0.hashCode(), user.hashCode());
     assertEquals("user is not same os original", user0, user);
-    // DELETEME: deep equals will not work because of the birtDate - db returns zeroes as time, new Date not!
-    assertFalse("reflection equals should not return true", EqualsBuilder.reflectionEquals(user0, user, true));
+
   }
 }

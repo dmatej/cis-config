c3ad7695bc03c638c0d78e467796e47577e57bd1
andrej <pirat.yohoho@gmail.com>
2014-04-29 17:34:44 +0200

    #37 missing namespace added

---
+++ cis-config-war/src/main/java/cz/i/cis/config/web/UserValidator.java
@@ -0,0 +1,27 @@
+package cz.i.cis.config.web;
+import java.security.Principal;
+
+import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+
+import cz.i.cis.config.ejb.dao.CisUserDao;
+import cz.i.cis.config.jpa.CisUser;
+
+@Named(value = "userValidator")
+public class UserValidator{
+  @EJB
+  private CisUserDao userDao;
+    public boolean loginExist(){
+  Principal currentUser = FacesContext.getCurrentInstance().getExternalContext().getUserPrincipal();
+  //FacesUtils.addMessage(FacesMessage.SEVERITY_INFO, currentUser.getName());
+  CisUser temp = userDao.getUser(currentUser.getName());
+        if(temp == null)
+            return false;
+        else if(temp.isDeleted())
+             return false;
+
+        return true;
+    }
+}

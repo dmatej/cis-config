144afdc14b207dff482a2b7ac886295594c46b61
augustar <augusta.rudolf@gmail.com>
2014-05-20 00:22:46 +0200

    #56 deleted wrong exception and dao cleaned. Unused method deleted.

--- cis-config-ejb/src/main/java/cz/i/cis/config/ejb/dao/ConfigurationItemKeyDao.java
+++ cis-config-ejb/src/main/java/cz/i/cis/config/ejb/dao/ConfigurationItemKeyDao.java
@@ -12,100 +12,45 @@ import javax.ejb.TransactionManagement;
 import javax.ejb.TransactionManagementType;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.PersistenceException;
 import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import cz.i.cis.config.ejb.dao.exceptions.ActiveItemKeyException;
-import cz.i.cis.config.ejb.dao.exceptions.UniqueKeyException;
 import cz.i.cis.config.jpa.ConfigurationItem;
 import cz.i.cis.config.jpa.ConfigurationItemCategory;
 import cz.i.cis.config.jpa.ConfigurationItemKey;
-
 
 @Local
 @Stateless
 @TransactionManagement(TransactionManagementType.CONTAINER)
 public class ConfigurationItemKeyDao {
 
-  private static final Logger LOG = LoggerFactory.getLogger(ConfigurationItemKeyDao.class);
-
   @PersistenceContext(name = "cis-jta")
   private EntityManager em;
 
 
+  public ConfigurationItemKey getItemKey(Integer id) {
+    return em.find(ConfigurationItemKey.class, id);
+  }
+
+
   public List<ConfigurationItemKey> listItemKeys() {
-    LOG.debug("listItemKeys()");
-    final TypedQuery<ConfigurationItemKey> query = this.em.createQuery(
-        "select itemKey from ConfigurationItemKey itemKey", ConfigurationItemKey.class);
+    final TypedQuery<ConfigurationItemKey> query = em.createQuery("SELECT itemKey FROM ConfigurationItemKey itemKey",
+        ConfigurationItemKey.class);
 
     return query.getResultList();
   }
 
 
-  @TransactionAttribute(TransactionAttributeType.REQUIRED)
-  public void addItemKey(ConfigurationItemKey key) throws UniqueKeyException {
-    LOG.debug("addItemKey(key={})", key);
-    try {
-      this.em.persist(key);
-      this.em.flush();
-    } catch (PersistenceException exc) {
-      throw new UniqueKeyException("Key" + key.getKey() + " already exists!", exc);
-    }
-  }
-
-
-  @TransactionAttribute(TransactionAttributeType.REQUIRED)
-  public void removeItemKey(ConfigurationItemKey key) {
-    LOG.debug("removeItemKey(key={})", key);
-    this.em.remove(key);
-  }
-
-
-  public ConfigurationItemKey updateItemKey(ConfigurationItemKey key) {
-    LOG.debug("updateItemKey(key={})", key);
-    return this.em.merge(key);
-  }
-
-
   public List<ConfigurationItemKey> filterItemKeys(ConfigurationItemCategory category) {
-    LOG.debug("filterItemKeys(category={})", category);
-    category = this.em.getReference(ConfigurationItemCategory.class, category.getId());
+    category = em.getReference(ConfigurationItemCategory.class, category.getId());
 
-    final TypedQuery<ConfigurationItemKey> query = this.em.createQuery(
+    final TypedQuery<ConfigurationItemKey> query = em.createQuery(
         "SELECT itemKey FROM ConfigurationItemKey itemKey WHERE itemKey.category = :category",
         ConfigurationItemKey.class);
     query.setParameter("category", category);
 
     return query.getResultList();
-  }
-
-
-  @TransactionAttribute(TransactionAttributeType.REQUIRED)
-  public void removeItemKey(Integer id) throws ActiveItemKeyException {
-    ConfigurationItemKey itemKey = getItemKey(id);
-
-    final TypedQuery<ConfigurationItem> activeQuery = this.em.createQuery(
-        "SELECT item FROM ConfigurationItem item WHERE item.key = :itemKey", ConfigurationItem.class);
-    activeQuery.setParameter("itemKey", itemKey);
-
-    if (!activeQuery.getResultList().isEmpty()) {
-      throw new ActiveItemKeyException("Key " + itemKey.getKey() + " is used in an active configuration!");
-    }
-
-    final Query profileDeleteQuery = this.em.createQuery("DELETE FROM ConfigurationProfileItem item WHERE item.key = :itemKey");
-    profileDeleteQuery.setParameter("itemKey", itemKey);
-    profileDeleteQuery.executeUpdate();
-
-    this.em.remove(itemKey);
-  }
-
-
-  public ConfigurationItemKey getItemKey(Integer id) {
-    return this.em.find(ConfigurationItemKey.class, id);
   }
 
 
@@ -116,5 +61,36 @@ public class ConfigurationItemKeyDao {
     }
 
     return itemKeyMap;
+  }
+
+
+  @TransactionAttribute(TransactionAttributeType.REQUIRED)
+  public void addItemKey(ConfigurationItemKey key) {
+    em.persist(key);
+  }
+
+
+  public ConfigurationItemKey updateItemKey(ConfigurationItemKey key) {
+    return em.merge(key);
+  }
+
+
+  @TransactionAttribute(TransactionAttributeType.REQUIRED)
+  public void removeItemKey(Integer id) throws ActiveItemKeyException {
+    ConfigurationItemKey itemKey = getItemKey(id);
+
+    final TypedQuery<ConfigurationItem> activeQuery = em.createQuery(
+        "SELECT item FROM ConfigurationItem item WHERE item.key = :itemKey", ConfigurationItem.class);
+    activeQuery.setParameter("itemKey", itemKey);
+
+    if (!activeQuery.getResultList().isEmpty()) {
+      throw new ActiveItemKeyException("Key " + itemKey.getKey() + " is used in an active configuration!");
+    }
+
+    final Query profileDeleteQuery = em.createQuery("DELETE FROM ConfigurationProfileItem item WHERE item.key = :itemKey");
+    profileDeleteQuery.setParameter("itemKey", itemKey);
+    profileDeleteQuery.executeUpdate();
+
+    em.remove(itemKey);
   }
 }

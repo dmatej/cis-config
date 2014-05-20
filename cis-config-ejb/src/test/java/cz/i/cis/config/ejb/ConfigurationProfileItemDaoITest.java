a107a1fef207c43e5994b3941a4edfd78ed5877c
andrej <pirat.yohoho@gmail.com>
2014-05-20 12:21:02 +0200

    #57 ConfigurationProfileItemDaoITest was updated and there are was added new tests

--- cis-config-ejb/src/test/java/cz/i/cis/config/ejb/ConfigurationProfileItemDaoITest.java
+++ cis-config-ejb/src/test/java/cz/i/cis/config/ejb/ConfigurationProfileItemDaoITest.java
@@ -4,8 +4,10 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 import javax.ejb.EJB;
 import javax.ejb.TransactionAttribute;
@@ -24,6 +26,7 @@ import cz.i.cis.config.ejb.dao.ConfigurationItemKeyDao;
 import cz.i.cis.config.ejb.dao.ConfigurationProfileDao;
 import cz.i.cis.config.ejb.dao.ConfigurationProfileItemDao;
 import cz.i.cis.config.ejb.dao.exceptions.CisUserDaoException;
+import cz.i.cis.config.ejb.dao.exceptions.ConfigurationProfileItemDaoException;
 import cz.i.cis.config.helpers.ConfigurationCategoryTestHelper;
 import cz.i.cis.config.helpers.ConfigurationItemKeyTestHelper;
 import cz.i.cis.config.helpers.ConfigurationProfileItemTestHelper;
@@ -47,7 +50,7 @@ public class ConfigurationProfileItemDaoITest extends ArquillianITest {
   @EJB(mappedName = "java:global/cis-config-test/cis-config-test-ejb/ConfigurationItemKeyDao")
   private ConfigurationItemKeyDao configItemkeyDao;
 
-  @EJB(mappedName = "java:global/cis-config-test/cis-config-test-ejb/ConfigurationCategoryDao")
+  @EJB(mappedName = "java:global/cis-config-test/cis-config-test-ejb/ConfigurationItemCategoryDao")
   private ConfigurationItemCategoryDao categoryDao;
 
   @EJB(mappedName = "java:global/cis-config-test/cis-config-test-ejb/ConfigurationProfileDao")
@@ -89,7 +92,7 @@ public class ConfigurationProfileItemDaoITest extends ArquillianITest {
 
 
   @Test
-  public void creatNewConfigurationProfileItem() throws CisUserDaoException {
+  public void creatNewConfigurationProfileItem() throws CisUserDaoException, ConfigurationProfileItemDaoException {
     final ConfigurationItemCategory category = new ConfigurationItemCategory();
     category.setName("some category name");
     categoryDao.addCategory(category);
@@ -134,8 +137,9 @@ public class ConfigurationProfileItemDaoITest extends ArquillianITest {
   }
 
 
-  @Test(expected = PersistenceException.class)
-  public void creatAlreadyExistingConfigurationProfileItem() throws PersistenceException {
+  @Test(expected = ConfigurationProfileItemDaoException.class)
+  public void creatAlreadyExistingConfigurationProfileItem() throws PersistenceException,
+    ConfigurationProfileItemDaoException {
     final ConfigurationProfileItem profileItem = profileItemHelper.createConfigurationProfileItem();
     final ConfigurationProfileItem profileItemCopy = new ConfigurationProfileItem();
     profileItemCopy.setProfile(profileItem.getProfile());
@@ -145,12 +149,76 @@ public class ConfigurationProfileItemDaoITest extends ArquillianITest {
   }
 
 
+  @Test(expected = ConfigurationProfileItemDaoException.class)
+  public void updateWrongConfigurationProfileItem() throws PersistenceException, ConfigurationProfileItemDaoException {
+    final ConfigurationItemCategory category = new ConfigurationItemCategory();
+    category.setId(1);
+    final ConfigurationItemKey key = new ConfigurationItemKey();
+    key.setId(1);
+    key.setCategory(category);
+    final CisUser user = new CisUser();
+    user.setId(1);
+    final ConfigurationProfile profile = new ConfigurationProfile();
+    profile.setId(1);
+    profile.setUser(user);
+    final ConfigurationProfileItem profileItem = new ConfigurationProfileItem();
+    profileItem.setId(1);
+    profileItem.setKey(key);
+    profileItem.setProfile(profile);
+    profileItem.setId(1);
+
+    profileItemDao.updateItem(profileItem);
+  }
+
+
+  @Test(expected = ConfigurationProfileItemDaoException.class)
+  public void testRemoveConfigurationProfileItem() throws PersistenceException, ConfigurationProfileItemDaoException {
+    final ConfigurationItemCategory category = new ConfigurationItemCategory();
+    category.setId(1);
+    final ConfigurationItemKey key = new ConfigurationItemKey();
+    key.setId(1);
+    key.setCategory(category);
+    final CisUser user = new CisUser();
+    user.setId(1);
+    final ConfigurationProfile profile = new ConfigurationProfile();
+    profile.setId(1);
+    profile.setUser(user);
+    final ConfigurationProfileItem profileItem = new ConfigurationProfileItem();
+    profileItem.setId(1);
+    profileItem.setKey(key);
+    profileItem.setProfile(profile);
+
+    profileItemDao.removeItem(profileItem);
+  }
+
+
   @Test
   public void listConfigurationProfileItems() {
     final List<ConfigurationProfileItem> items = profileItemDao.listItems();
     LOG.debug("list configuration profile items: {}", items);
     assertNotNull("configuration profile items", items);
     assertTrue("items.empty", items.isEmpty());
+
+    final ConfigurationProfileItem profileItem = profileItemHelper.createConfigurationProfileItem();
+    int profileId = profileItem.getProfile().getId();
+    final List<ConfigurationProfileItem> itemsId = profileItemDao.listItems(profileId);
+    LOG.debug("list configuration profile items by profile id: {}", itemsId);
+    assertNotNull("configuration profile items y profile id", itemsId);
+    assertTrue("itemsId.size == 1", itemsId.size() == 1);
+  }
+
+
+  @Test
+  public void testSaveChanges() throws ConfigurationProfileItemDaoException {
+    final ConfigurationProfileItem profileItem1 = profileItemHelper.createConfigurationProfileItem();
+    final ConfigurationProfileItem profileItem2 = profileItemHelper.createConfigurationProfileItem();
+    List<ConfigurationProfileItem> items = new ArrayList<ConfigurationProfileItem>();
+    items.add(profileItem1);
+    items.add(profileItem2);
+    Map<String, ConfigurationProfileItem> profileItems = ConfigurationProfileItemDao.getItemMap(items);
+    assertTrue("profileItems.size == 2", profileItems.size() == 2);
+    profileItemDao.saveChanges(profileItems);
+    assertTrue("profileItems.size == 2", profileItems.size() == 2);
   }
 
 
